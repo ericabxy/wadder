f30b33e5f40f0aecdf2fb5a9f5d961b7af9b1cbc
Eric Duhamel <ericxdu23@gmail.com>
2022-01-23 00:04:09 -0800

    make patter.py work directly with binary colormaps

--- patter.py
+++ patter.py
@@ -24,13 +24,16 @@ def main():
             check_file(filename)
         colormap = graymap()
         for arg in sys.argv:
-            if arg[0:14] == "--read-hexmap=":
-                path = arg[14:]
-                colormap = read_hexmap(path)
+            if arg[0:11] == "--colormap=":
+                path = arg[11:]
+                colormap = load_colormap(path)
             elif arg == "--save-pixmap":
-                picture, width, height = load_picture(filename, colormap)
+                width, height, left, top, columnofs = parse_header(filename)
+                with open(filename, 'rb') as file:
+                    bytemap, bitmask = get_patch(file, width, height, columnofs)
+                pixmap = convert(bytemap, bitmask, colormap)
                 name = os.path.splitext(filename)[0]
-                save_pixmap(picture, name + ".ppm", width, height)
+                save_pixmap(pixmap, width, height, name + ".ppm")
 
 def check_file(filename, maxw=256):
     with open(filename, 'rb') as file:
@@ -71,7 +74,51 @@ def check_file(filename, maxw=256):
                                 print("    post", j, end=": ")
                                 print(bin(data[j])[2:])
 
-def get_header(filename):
+def convert(bytemap, bitmask, colormap):
+    pixmap = bytearray()
+    for b, m in zip(bytemap, bitmask):
+        if m == 0:
+            pixmap.append(255)
+            pixmap.append(0)
+            pixmap.append(255)
+        else:
+            pixmap.append(colormap[b*3])
+            pixmap.append(colormap[b*3+1])
+            pixmap.append(colormap[b*3+2])
+    return pixmap
+
+def get_patch(file, width, height, columnofs):
+    bytemap = bytearray(width*height)
+    bitmask = [0 for i in range(width*height)]
+    for i, offset in enumerate(columnofs):
+        file.seek(offset)
+        topdelta = 0
+        while topdelta < 255:  # read posts until terminator
+            topdelta = int.from_bytes(file.read(1), byteorder='little')
+            length = int.from_bytes(file.read(1), byteorder='little')
+            unused1 = int.from_bytes(file.read(1), byteorder='little')
+            data = file.read(length)
+            unused2 = int.from_bytes(file.read(1), byteorder='little')
+            if topdelta < 255:
+                for j in range(length):
+                    bytemap[i+(topdelta*width)+(j*width)] = data[j]
+                    bitmask[i+(topdelta*width)+(j*width)] = 1
+    return bytemap, bitmask
+
+def graymap():
+    map = bytearray()
+    for i in range(256):
+        map.append(i)
+        map.append(i)
+        map.append(i)
+    return map
+
+def load_colormap(filename):
+    """Load a 24bpp color translation map."""
+    with open(filename, 'rb') as file:
+        return file.read(768)
+
+def parse_header(filename):
     """Return each part of the file header in a dictionary."""
     with open(filename, 'rb') as file:
         width = int.from_bytes(file.read(2), byteorder='little')
@@ -81,59 +128,15 @@ def get_header(filename):
         columnofs = []
         for i in range(width % 256):
             columnofs.append(int.from_bytes(file.read(4), byteorder='little'))
-    return dict(width=width,height=height,leftoffset=leftoffset,
-            topoffset=topoffset,columnofs=columnofs)
+    return width, height, leftoffset, topoffset, columnofs
 
-def graymap():
-    """Return 256 hex-encoded grayscale values."""
-    map = []
-    for i in range(256):
-        hexval = hex(i)[2:].zfill(2)
-        map.append(hexval + hexval + hexval + "\n")
-    return map
-
-def load_picture(filename, hexmap):
-    with open(filename, 'rb') as file:
-        width = int.from_bytes(file.read(2), byteorder='little')
-        height = int.from_bytes(file.read(2), byteorder='little')
-        leftoffset = int.from_bytes(file.read(2), byteorder='little')
-        topoffset = int.from_bytes(file.read(2), byteorder='little')
-        columnofs = []
-        for i in range(width % 256):
-            columnofs.append(int.from_bytes(file.read(4), byteorder='little'))
-        picture = ['ff00ff' for i in range(width*height)]
-        for i, offset in enumerate(columnofs):
-            file.seek(offset)
-            topdelta = 0
-            while topdelta < 255:  # read posts until terminator
-                topdelta = int.from_bytes(file.read(1), byteorder='little')
-                length = int.from_bytes(file.read(1), byteorder='little')
-                unused1 = int.from_bytes(file.read(1), byteorder='little')
-                data = file.read(length)
-                unused2 = int.from_bytes(file.read(1), byteorder='little')
-                for j in range(length):
-                    if topdelta < 255:
-                        color = hexmap[data[j]]
-                        picture[i+(topdelta*width)+(j*width)] = color
-    return picture, width, height
-
-def read_hexmap(filename):
-    """Return 256 hex-encoded values read from a file."""
-    map = []
-    with open(filename) as file:
-        for i in range(256):
-            map.append(file.readline())
-    return map
-
-def save_pixmap(hexmap, name, width=256, height=256):
-    """Save 3-byte hex values to a Portable PixMap."""
+def save_pixmap(bytemap, width, height, name):
     with open(name, 'wb') as file:
         file.write(b"P6 ")
         file.write(bytes(str(width) + " ", 'utf_8'))
         file.write(bytes(str(height) + " ", 'utf_8'))
         file.write(b"255 ")
-        for hex in hexmap:
-            file.write(bytes.fromhex(hex))
+        file.write(bytemap)
 
 if __name__ == "__main__":
     try: main()
